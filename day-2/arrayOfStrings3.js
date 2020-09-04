b3d56f36f031f2bcfb068049bd6d9b89f1b337bd
telumudhiraj <37935359+telumudhiraj@users.noreply.github.com>
2020-09-04 16:26:00 +0530

    Add files via upload

---
+++ day-2/arrayOfStrings3.js
@@ -0,0 +1,16 @@
+let d = ["sun", "moon", "earth", "saturn", "rings", "isro", "nasa", "space"];
+let searelemt = "isro";
+let c = 0;
+console.log("The element we are searching which is '" + searelemt + "' in array of strings '" + d + "' is at index ")
+for (let i = 0; i < d.length; i++) {
+    // c = 0;
+    // for (let j = 0; j < d[i].length; j++) {
+    if (d[i] == searelemt) {
+        //     c++;
+
+        // }
+        // if (c != 0) {
+        console.log(i);
+    }
+}
+// }
